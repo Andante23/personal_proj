03dcb013d180115259ce7065c6ba3af26bfd1b34
andan <andante1104@naver.com>
2024-01-05 22:09:29 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 'use strict';
 
-
 // options는 tmdb api 명세의 기본 틀에 존재하기에 필요합니다. 
 const options = {
     method: 'GET',  
@@ -11,70 +10,46 @@ const options = {
     }
 };
 
-
-
 // API는 변함이 없으므로 상수로 고정
 const apiKey = config.tmdbApi;
 
-// movieIds는  response 콘솔을 찍어서 나온 id로 함   
-const movieIds = [2,5,6,8,11,13,15,17,21];
+// fetch를 통해서 top_rated의 데이터를 받아옵시다. 
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(data => {     
+        const movieIds = data.results;
+        // console.log(data.results);  json 형식으로 변환된 top_rated 데이터를 콘솔에  퉤하고 뺃어줍니다. 배열이더라고여  
+        // console.log(typeof(movieIds));  자료형이 object입니다.  참고로  array도 객체도 오브젝트입니다.  
+        //  배열이기에 forEach가 가능한 것입니다. 
+        movieIds.forEach(movieId => {
+          
+            //  console.log(movieId); : movieIds의 요소를 movieId라하는데  잘 나왔는지 확인하는 것입니다. 
+            // 퉤 뱃어진 movieId를 자세히 보시면 아이디에 접근이 가능한 번호가 있습니다. 그것을  movieId.id라 합시다. 
+            // 다음 url은 영화(movie)에 대한 정보를 밷어주는 url입니다. 이것을 이용해서  top_rated 데이터를 불러옵시다.
 
-// movieIds를 반복으로 받으면서 카드를 붙여주는 과정을 반복한다.
-movieIds.forEach(movieId => {
-  
-   
+            const url = `https://api.themoviedb.org/3/movie/${movieId.id}?api_key=${apiKey}&language=en-US&page=1`;
 
-    // 영화카드를 보이게 하는  movieCardBlock 함수 정의합니다. 
-    function movieCardBlock(options){
-        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
-        
-        // options는 tmdbAPI 명세에서 요구하기에 가져왔습니다. 
-        fetch(url, options)
-        // 응답받을 것을 .json()을 이용해서 json형식으로 변환
-        .then(response => response.json()) 
-        .then(response => {
-         //  API서버로부터 응답받은 response 데이터를 확인하기 위해서 다음과 같이 콘솔 출력 
-         // console.log(response);
-    
-    /***
-        영화 이미지 : poster_path
-        
-        영화 제목 : original_title
-      
-        영화 내용 : overview
-      
-        영화 평점 : vote_average
-    */
-
-    // 해당 HTML 문서 ,index.html 의  movieListView  아이디선택자에 접근
-    // .innerHTML 메서드를 이용해서 html을 붙여넣기 함 
-    // 그런데 이과정이 한번이 아닌 여러번이어서 반복문을 사용함 
-    document.getElementById('movieListView').innerHTML += `
-     <div class="movieCard">
-       <img src="https://image.tmdb.org/t/p/original${response.poster_path} " alt="movie poster" class="mvImg" id="mv__img">
-    
-     <div class="movieCarte">
-       <p class="mvTitle"  id="mv__title">${response.original_title}</p>
-       <span class="mvRate"><small id="mv__Rate">${response.vote_average}</small></span>
-     </div>   
-
-       <p class="mvParam"  id="mv__param">${response.overview}</p> 
-       </div>`;
-    }).catch( 
-            err => console.error(err)  // 크롬 브라우저 개발자 도구 console 창에 에러 출력 
-            );
-    }  
-
-
-    // movieCardBlock 함수를 선언합니다. 
-    movieCardBlock(options);
-
-
-    });
-
-
-
-
+            fetch(url, options)
+                .then(response => response.json())
+                .then(response => {
+                   // console.log(response); : 제대로 봤아왔는지 확인하는 것입니다. 
+                    const movieListView = document.getElementById('movieListView');
+                    movieListView.innerHTML += `
+                        <div class="movieCard">
+                            <img src="https://image.tmdb.org/t/p/original${response.backdrop_path}" alt="movie poster" class="mvImg" id="mv__img">
+                            <div class="movieCarte">
+                                <p class="mvTitle" id="mv__title">${response.original_title}</p>
+                                <span class="mvRate"><small id="mv__Rate">${response.vote_average}</small></span>
+                            </div>
+                            <p class="mvParam" id="mv__param">${response.overview}</p> 
+                        </div>`;
+                })
+                // 내부 fetch에서 rated 영화 데이터를 못 가져오면 에러를  밷습니다. 
+                .catch(err => console.error(err));
+        });
+    })
+    // 또한 외부 fetch에서 rated 데이터를 못 가져오면 에러를  밷습니다. 
+    .catch(err => console.error(err));
 
 // 사용자가 검색어를 입력하면 그에 따라 카드를 보여주는 함수 searchPlay 함수
 const searchPlay = () => {
@@ -119,6 +94,8 @@ const searchPlay = () => {
 
 // searchBtn을 클릭하면  searchPlay함수를 동작시켜주세요 
 document.getElementById('searchBtn').addEventListener('click',searchPlay);
+
+
 
 
   
